d079c9a7a341cb5d7c9b0bc2a7b6c7d6500b8ec5
Sungwon Kim <sungwon.kim@cern.ch>
2024-08-02 01:11:48 +0900

    First version of analysis code running on CentOS7

--- buildNinstall_centos.sh
+++ buildNinstall_centos.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-source envset.sh
+source envset_centos.sh
 mkdir build install
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../install 
---
+++ envset_centos.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# When using CVMFS
+source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
+
+export INSTALL_DIR_PATH=$PWD/install
+
+export PATH=$PATH:$INSTALL_DIR_PATH/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INSTALL_DIR_PATH/lib64
+export DYLD_LIBRARY_PATH=$INSTALL_DIR_PATH/lib64
+export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR_PATH/lib64
+export YAMLPATH=/cvmfs/sft.cern.ch/lcg/releases/yamlcpp/0.6.3-d05b2/x86_64-centos7-gcc11-opt/lib
